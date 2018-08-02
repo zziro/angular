74442e1c2e54a5ded261ccf12696c537ad1157d1
Ciro Liberato <zziro.123@gmail.com>
2018-08-02 16:11:19 -0500

    Directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,6 +12,9 @@ export class ServersComponent implements OnInit {
   serverName = 'TestServer';
   allowButton = true;
   username = '';
+  serverCreated = false;
+  servers = ['TestServer','TestSever 2'];
+  
 
   constructor() { 
     setTimeout(() => {
@@ -23,14 +26,12 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created!' + this.serverName;
   }
 
   onUpdateServerName(event: any) {
     this.serverName = (<HTMLInputElement>event.target).value;
-  }
-
-  onResetValue (event: any) {
-    this.username = '';
   }
 }
